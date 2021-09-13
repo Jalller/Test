bd23f095db99bb573ef9cf5ea7fc9f813661b702
Jantie Larsen <cph-jl398@cphbusiness.dk>
2021-09-13 17:54:56 +0200

    NodeClass with variables

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,4 +1,18 @@
 package com.company;
 
-public class Node {
+public class Node
+{
+    public String name;
+
+
+    Node previous = null;
+    Node next = null;
+
+    public String getName() {
+        return name;
+    }
+
+    public Node(String name) {
+        this.name = name;
+    }
 }
